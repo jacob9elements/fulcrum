3fa6064dacdd1308a6f2ee9c9ed458f546b985a8
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2014-03-30 14:59:39 +0200

    Added production ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,14 @@ Fulcrum::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => config.fulcrum.app_host }
+  # ActionMailer configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address  => "ubermail.9elements.com",
+    :port  => 25,
+    :domain  => "9elements.com",
+    :enable_starttls_auto => false
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
